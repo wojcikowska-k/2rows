70e95c49c6e6efb3e54656666968a917b0f6b047
wojcikowska-k <116753173+wojcikowska-k@users.noreply.github.com>
2024-02-13 20:02:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 2rows
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/wojcikowska-k/2rows)
---
+++ index.js
@@ -0,0 +1,19 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `
+<div>
+<ul>
+<li>Item</li>
+<li>Item</li>
+<li>Item</li>
+<li>Item</li>
+<li>Item</li>
+<li>Item</li>
+<li>Item</li>
+<li>Item</li>
+</ul>
+</div>
+`;
